e3c0de1348ce374bce9edd06a699c93e1a214f4b
Nikita Duzhnov <duzhnov.nikita55@mail.ru>
2024-06-30 16:32:27 +0300

    добавление виртального окружения

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print("hello")
+from dataclasses import dataclass
 
 
-print("say hello world!")
+@dataclass
+class User:
+    user_id: int
+    name: str
+    age: int
+    email: str
+
+
+def get_user_info(user: User) -> str:
+    return f'Возраст пользователя {user.name} - {user.age}, ' \
+           f'а email - {user.email}'
+
+
+user_1: User = User(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
+print(get_user_info(user_1))
