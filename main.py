89bf94a89ed9f1b882a1e5e388ad062b3f11a1dc
Nikita Duzhnov <duzhnov.nikita55@mail.ru>
2024-06-30 16:49:47 +0300

    добавли httpx и протестил

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from dataclasses import dataclass
+import httpx
 
 
-@dataclass
-class User:
-    user_id: int
-    name: str
-    age: int
-    email: str
+api_url = 'http://api.open-notify.org/iss-now.json'
 
+response = httpx.get(api_url)
 
-def get_user_info(user: User) -> str:
-    return f'Возраст пользователя {user.name} - {user.age}, ' \
-           f'а email - {user.email}'
+if response.status_code ==200:
+    print(response.text)
 
-
-user_1: User = User(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
-print(get_user_info(user_1))
+else:
+    print(response.status_code)
