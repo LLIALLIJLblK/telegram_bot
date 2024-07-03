9b5fa47a776c6d9c170ab6f6705a1869409b2458
Nikita Duzhnov <duzhnov.nikita55@mail.ru>
2024-07-03 19:20:28 +0300

    добавил логику

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import httpx
 
 
-api_url = 'http://api.open-notify.org/iss-now.json'
+#api_url = 'https://api.telegram.org/bot7035854081:AAFjHzxsT_vsDYsHXhVPRnctcq2RBg_A8mA/sendMessage&text=Привет,"username" !'
+api_url = 'https://api.telegram.org/bot7035854081:AAFjHzxsT_vsDYsHXhVPRnctcq2RBg_A8mA/sendMessage?chat_id=887577092&text=hello'
 
 response = httpx.get(api_url)
 
 if response.status_code ==200:
-    print(response.text)
+    data = response.text
+    print(data)
 
 else:
     print(response.status_code)
