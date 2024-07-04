2f0c23a8dc4172123c3ea733621f66ef761d65a8
Nikita Duzhnov <duzhnov.nikita55@mail.ru>
2024-07-04 09:37:01 +0300

    бот отправляет котиков

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-import httpx
+import httpx #ДЛЯ РЕКВЕСТОВ
+import time
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '7035854081:AAFjHzxsT_vsDYsHXhVPRnctcq2RBg_A8mA'
+TEXT = 'Новый апдейт'
+MAX_COUNTER = 100
+API_CAT = 'https://api.thecatapi.com/v1/images/search'
+
+offset = -2
+counter = 0
+chat_id: int
+
+while counter < MAX_COUNTER:
+    print(f"attempt = {counter}")
+
+    updates = httpx.get(f"{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}").json()
+    cat_photo = httpx.get("https://api.thecatapi.com/v1/images/search").json()
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = httpx.get(API_CAT)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                httpx.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                httpx.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={"error"}')
 
 
-#api_url = 'https://api.telegram.org/bot7035854081:AAFjHzxsT_vsDYsHXhVPRnctcq2RBg_A8mA/sendMessage&text=Привет,"username" !'
-api_url = 'https://api.telegram.org/bot7035854081:AAFjHzxsT_vsDYsHXhVPRnctcq2RBg_A8mA/sendMessage?chat_id=887577092&text=hello'
 
-response = httpx.get(api_url)
-
-if response.status_code ==200:
-    data = response.text
-    print(data)
-
-else:
-    print(response.status_code)
+    time.sleep(1)
+    counter += 1
